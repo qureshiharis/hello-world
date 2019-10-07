4019b7ecfa2ee74d415c6f08c159769b16fdf673
qureshiharis <haris.qureshi96@gmail.comgit config --global user.name qureshiharisgit add hello.htmlgit commit -m Added>
2019-10-07 15:56:01 +0500

    Added two functions

--- first_python_file.py
+++ first_python_file.py
@@ -6,3 +6,13 @@ subprocess.Popen("msg * YOU HAVE BEEN HACKED", shell=True)
 # It doesnt work on my terminal, it says /bin/sh: 1: msg: not found
 
 #hmmmm! try it on windows if its running!
+
+def sum(x, y):
+    # TODO Waqar
+
+    return value
+
+def sub(x, y):
+    # TODO Haris
+    value = y-x
+    return value
