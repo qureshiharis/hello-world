efe09bbfdf951081aaeb4d9a128ff3b8536a3657
Waqar-Arain <adil.cn85@gmail.com>
2019-10-07 15:49:00 +0500

    3rd commit

--- first_python_file.py
+++ first_python_file.py
@@ -4,3 +4,5 @@ import subprocess
 subprocess.Popen("msg * YOU HAVE BEEN HACKED", shell=True)
 
 # It doesnt work on my terminal, it says /bin/sh: 1: msg: not found
+
+#hmmmm! try it on windows if its running!
